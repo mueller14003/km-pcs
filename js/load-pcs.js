f7db66e7fb20971b713893baa307e43a94263d94
Moses Apostol <mapo3126@gmail.com>
2022-09-25 16:48:46 -0600

    Simplified js by making var for pc grid

--- js/load-pcs.js
+++ js/load-pcs.js
@@ -1,17 +1,18 @@
 function loadPCs() {
-    document.getElementById("pc_grid").innerHTML = "";
+    var pc_grid = document.getElementById("pc_grid");
+    pc_grid.innerHTML = "";
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         var json_obj = JSON.parse(this.responseText);
         for (x in json_obj.PCs) {
           PC = json_obj.PCs[x];
-          document.getElementById("pc_grid").innerHTML += formatPC(PC);
+          pc_grid.innerHTML += formatPC(PC);
         }
-        document.getElementById("pc_grid").style = "";
+        pc_grid.style = "";
       } else if (this.status == 404) {
-        document.getElementById("pc_grid").style.color = "red";
-        document.getElementById("pc_grid").innerHTML = "File Not Found?!?";
+        pc_grid.style.color = "red";
+        pc_grid.innerHTML = "File Not Found?!?";
       }
     }
     xhttp.open("GET", "./PCs.json", true);
